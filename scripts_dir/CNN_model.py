46396276dcac2c017f5ff27783fa9e11a3b42fcd
Eliad Bar Shalom <eliad@LapVPN-eliadbs.local>
2024-02-25 09:10:00 +0200

    added comments and documentation

--- scripts_dir/CNN_model.py
+++ scripts_dir/CNN_model.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 
 class SimpleCNN(nn.Module):
     def __init__(self):
+        '''
+        This is te initializing function of a simple cnn model
+        '''
         super(SimpleCNN, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
@@ -11,6 +14,11 @@ class SimpleCNN(nn.Module):
         self.fc2 = nn.Linear(128, 10)
 
     def forward(self, x):
+        '''
+        This is the forward function for each loop in the training
+        :param x: one level before in the train
+        :return: the next training level
+        '''
         x = self.pool(torch.relu(self.conv1(x)))
         x = self.pool(torch.relu(self.conv2(x)))
         x = x.view(-1, 32 * 8 * 8)
