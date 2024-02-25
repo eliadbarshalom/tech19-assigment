d76d895ba1568f4d96a196b0c1dd959341d48d16
Eliad Bar Shalom <eliad@LapVPN-eliadbs.local>
2024-02-25 09:02:33 +0200

    added comments and minor fixes

--- scripts_dir/evaluate_model.py
+++ scripts_dir/evaluate_model.py
@@ -26,19 +26,18 @@ def compare_test_to_model_results(test_loader:DataLoader, model:nn.Module):
     return res
 
 
-#todo - need to do more generic
-def check_different_epoch_numbers(epoch_numbers):
+def check_different_epoch_numbers(epoch_numbers, loader):
     '''
-    
-    :param epoch_numbers:
-    :return:
+    This function is using for comparing different number of epochs for te model training
+    :param epoch_numbers:array of epoch numbers to check
+    :param loader: the loader to et te trest set from
+    :return:the model results on the loader test with the specific epoch number
     '''
-    loader = Loaders(data_path='/home/eliad/github_repo/tech19-home-assigment/data_dir')
     best_res = -1
     for i in epoch_numbers:
         model = SimpleCNN()
         run_training(train_loader=loader.train_loader, val_loader=loader.val_loader, model=model, num_epochs=i)
-        print(f'nuber of epochs:{i}')
+        print(f'number of epochs:{i}')
         res = compare_test_to_model_results(loader.test_loader, model)
         if res > best_res:
             best_res = res
@@ -46,4 +45,5 @@ def check_different_epoch_numbers(epoch_numbers):
 
 
 if __name__ == '__main__':
-    check_different_epoch_numbers(range(11, 15))
+    loader = Loaders(data_path='/home/eliad/github_repo/tech19-home-assigment/data_dir')
+    check_different_epoch_numbers(range(11, 15),loader)
